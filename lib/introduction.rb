6361b75ae4d45435468436fa8cd128e5caa1cdaa
Lola Onanuga <slonanuga@gmail.com>
2018-08-22 19:06:00 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,4 @@
 def introduction(name)
-  puts "Hi, name name is #{name}."
-  end
   
-  def introduction_with_language(name,language)
-  puts "Hi, my name is #{name} and I am learning to program n #{language}."
   end
+
